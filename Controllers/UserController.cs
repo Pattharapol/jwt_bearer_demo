e1c8b2c60f7b89431b09aab9c313a99eab87f9a3
phattharapol@izpal <phattharapol@izpal.com>
2022-05-17 11:20:31 +0700

    demo

---
+++ Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using JWT_authen.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JWT_authen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.GivenName}, you are on {currentUser.Role}");
+        }
+
+        [HttpGet("Sellers")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("AdminsAndSellers")]
+        [Authorize(Roles = "Administrator,Seller")]
+        public IActionResult AdminsAndSellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.GivenName}, you are on {currentUser.Role}");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaim = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaim.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaim.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaim.FirstOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaim.FirstOrDefault(x => x.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaim.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                };
+            }
+
+            return null;
+        }
+    }
+}
